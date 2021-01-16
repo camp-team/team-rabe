4c54a00f7ac21de2f7a631d594d3baeb3fa0c1fc
Robinson-R1127 <rabe.robinson01@gmail.com>
2021-01-16 15:16:06 +0900

    feat(header): header作成 #2

--- src/app/interfaces/user-data.ts
+++ src/app/interfaces/user-data.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 export interface UserData {
   uId: string;
   name: string;
-  avatarURL: string;
+  avatarURL?: string;
   activeRoomId: string;
   joinedRoomIds: string[];
   createdAt: admin.firestore.Timestamp;
