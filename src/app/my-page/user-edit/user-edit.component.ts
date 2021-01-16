253d1709feefbeca0639ad972e3ab28e28cb150a
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-16 23:26:48 +0900

    feat(user-edit): ユーザーネーム編集機能実装 #30

--- src/app/my-page/user-edit/user-edit.component.ts
+++ src/app/my-page/user-edit/user-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { UserData } from 'src/app/interfaces/user-data';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,7 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.scss'],
 })
 export class UserEditComponent implements OnInit {
-  constructor() {}
+  private uid: string = this.authServie.uid;
+
+  nameForm: FormControl = this.fb.control('', [
+    Validators.required,
+    Validators.maxLength(60),
+  ]);
+  user$: Observable<UserData> = this.authServie.user$;
+
+  constructor(
+    private fb: FormBuilder,
+    private authServie: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {}
+
+  changeUserName(): void {
+    const newUserName: string = this.nameForm.value;
+    this.userService.changeUserName(this.uid, newUserName);
+  }
 }
