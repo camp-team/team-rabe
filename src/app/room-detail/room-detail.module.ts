cfc3f55369e7a81028239553369264889bf19410
Yusuke Gunji <inochideutae@gmail.com>
2021-01-16 19:22:30 +0900

    feat(room-detail): add mat-side-nav

--- src/app/room-detail/room-detail.module.ts
+++ src/app/room-detail/room-detail.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RoomDetailRoutingModule } from './room-detail-routing.module';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
+import { SidenavRightComponent } from './sidenav-right/sidenav-right.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [RoomDetailComponent],
-  imports: [CommonModule, RoomDetailRoutingModule],
+  declarations: [RoomDetailComponent, SidenavRightComponent],
+  imports: [
+    CommonModule,
+    RoomDetailRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
 })
 export class RoomDetailModule {}
--- src/app/room-detail/room-detail/room-detail.component.ts
+++ src/app/room-detail/room-detail/room-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-room-detail',
@@ -6,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-detail.component.scss'],
 })
 export class RoomDetailComponent implements OnInit {
-  constructor() {}
+  opened$: Observable<boolean> = this.uiService.isOpen$;
+  scrWidth: any;
 
-  ngOnInit(): void {}
+  constructor(private uiService: UiService) {}
+
+  ngOnInit(): void {
+    this.getScreenSize();
+  }
+
+  toggleNav(): void {
+    this.uiService.toggleOpening();
+    console.log('toggle');
+  }
+
+  getScreenSize(): void {
+    this.scrWidth = window.innerWidth;
+  }
 }
