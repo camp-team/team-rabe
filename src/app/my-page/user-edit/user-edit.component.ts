d4d523d849cf4eed89d868b2978535a71cdda8e9
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-17 02:09:38 +0900

    feat(user-edit): ユーザーアバター画像編集機能実装 #30

--- src/app/my-page/user-edit/user-edit.component.ts
+++ src/app/my-page/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { UserData } from 'src/app/interfaces/user-data';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
@@ -18,6 +19,7 @@ export class UserEditComponent implements OnInit {
     Validators.maxLength(60),
   ]);
   user$: Observable<UserData> = this.authServie.user$;
+  oldImageUrl: string;
 
   constructor(
     private fb: FormBuilder,
@@ -25,10 +27,23 @@ export class UserEditComponent implements OnInit {
     private userService: UserService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getOldImageUrl();
+  }
+
+  private getOldImageUrl(): void {
+    this.user$
+      .pipe(take(1))
+      .toPromise()
+      .then((user: UserData) => (this.oldImageUrl = user.avatarURL));
+  }
 
   changeUserName(): void {
     const newUserName: string = this.nameForm.value;
     this.userService.changeUserName(this.uid, newUserName);
+  }
+
+  changeUserAvatar(image: string): void {
+    this.userService.changeUserAvatar(this.uid, image);
   }
 }
