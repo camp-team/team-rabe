f5da48b7470dbd909f6d06d7d8f8a62dca7e2f57
kokiinoue45 <koki_inoue45@icloud.com>
2021-01-16 21:54:51 +0900

    (機能,UI)Room-Edit実装 Fixes #29

---
+++ src/app/my-page/room-edit/room-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-edit',
+  templateUrl: './room-edit.component.html',
+  styleUrls: ['./room-edit.component.scss'],
+})
+export class RoomEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
