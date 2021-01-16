3fedc1dcbc1fcbe2191ddfacdc7ad251ed6328db
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-16 22:03:15 +0900

    feat(user-data): ユーザーデータ情報修正 #30

--- src/app/interfaces/user-data.ts
+++ src/app/interfaces/user-data.ts
@@ -4,7 +4,7 @@ export interface UserData {
   uId: string;
   name: string;
   avatarURL?: string;
-  activeRoomId: string;
+  activeRoomId?: string;
   joinedRoomIds: string[];
   createdAt: admin.firestore.Timestamp;
   updatedAt: admin.firestore.Timestamp;
