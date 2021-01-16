93809e4a0caec6f65a329463727238e21ef44e29
Takumi Uchida <bmpni23786@yahoo.co.jp>
2021-01-16 22:06:48 +0900

    feat(user.service): ユーザーネーム変更メソッド追加 #30

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserData } from '../interfaces/user-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  constructor(private db: AngularFirestore) {}
+
+  changeUserName(uid: string, name: string): Promise<void> {
+    return this.db.doc<UserData>(`users/${uid}`).update({ name });
+  }
 }
