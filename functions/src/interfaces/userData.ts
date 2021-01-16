a5dc14b79b239e96dc466a0abdc99560297c022f
Yusuke Gunji <inochideutae@gmail.com>
2021-01-17 03:06:57 +0900

    feat(userData): add interface

---
+++ functions/src/interfaces/userData.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin';
+
+export interface UserData {
+  uId: string;
+  name: string;
+  avatarURL?: string;
+  activeRoomId: string;
+  joinedRoomIds: string[];
+  createdAt: admin.firestore.Timestamp;
+  updatedAt: admin.firestore.Timestamp;
+}
